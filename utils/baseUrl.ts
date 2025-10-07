840be9a9dc2262c1f8dca7aa32c55b98b6cac075
vishvajeet <djritikshukla@gmail.com>
2025-10-07 15:31:48 +0530

    fgf

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -3,7 +3,7 @@
 // export const USER_URL ="https://serversql-brown.vercel.app/api/user"
 // export const Teacher_URL ="https://serversql-brown.vercel.app/api/teacher"
 // export const Notification_URL = "https://serversql-brown.vercel.app/api/notifications"
-// export const Student_URL ="https://serversql-brown.vercel.app/api/student"
+// exportconst Student_URL ="https://serversql-brown.vercel.app/api/student"
 
 
 export const Serverurl="https://serversql-ek4h.onrender.com"
